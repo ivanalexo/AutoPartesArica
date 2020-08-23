d95b0ea28b8ab2285fd9b40e93982277721b6ff2
Ivan Omonte Carreño <ivan.omonte@jalasoft.com>
2020-08-23 01:16:49 -0400

    add sales model

---
+++ app/models/sales.model.js
@@ -0,0 +1,37 @@
+module.exports = mongoose => {
+    const schema = mongoose.Schema(
+        {
+            product: [
+                {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'products'
+                }
+            ],
+            finalPrice: {
+                type: Number,
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            },
+            client: {
+                type: String,
+                required: true
+            },
+            salesman: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'user'
+            },
+        },
+        { timestamps: true }
+    );
+    schema.method('toJSON', function () {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+    const Sale = mongoose.model('sale', schema);
+    return Sale;
+}
